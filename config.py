2cc49cb10461ea660813a2bab774499f8efb0259
dogukansagir <dogukanasddsa@gmail.com>
2026-05-20 21:57:10 +0300

    defined models and api keys in config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+DEEPSEEK_API_URL = "https://api.deepseek.com"
+DEEPSEEK_MODEL = "deepseek-v4-flash"
+DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
+TAVILY_API_KEY= os.getenv("TAVILY_API_KEY")
+LANGFUSE_PUBLIC_KEY= os.getenv("LANGFUSE_PUBLIC_KEY")
+LANGFUSE_SECRET_KEY= os.getenv("LANGFUSE_SECRET_KEY")
+LANGFUSE_HOST="https://cloud.langfuse.com"
+
+if not DEEPSEEK_API_KEY:
+    raise ValueError("DEEPSEEK_API_KEY is not set")
+if not TAVILY_API_KEY:
+    raise ValueError("TAVILY_API_KEY is not set")
